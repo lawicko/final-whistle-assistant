986267c00d24d84b811f6a42bd1909cb0561d4c9
Jakub Lawicki <lawicko+github@gmail.com>
2025-10-07 14:01:31 +0200

    Tag selector colors now obey the customized color schemes

--- content-scripts/tags.js
+++ content-scripts/tags.js
@@ -9,31 +9,31 @@ async function applyCustomColorsForTags() {
         if (!colors) return
         // Inject CSS rule so future elements are styled too
         addCSS(`
-            i.denom1, span.denom1 i {
+            i.denom1, span.denom1 i, div.denom1 {
                 color: ${colors.tagColor1} !important;
             }
-            i.denom2, span.denom2 i {
+            i.denom2, span.denom2 i, div.denom2 {
                 color: ${colors.tagColor2} !important;
             }
-            i.denom3, span.denom3 i {
+            i.denom3, span.denom3 i, div.denom3 {
                 color: ${colors.tagColor3} !important;
             }
-            i.denom4, span.denom4 i {
+            i.denom4, span.denom4 i, div.denom4 {
                 color: ${colors.tagColor4} !important;
             }
-            i.denom5, span.denom5 i {
+            i.denom5, span.denom5 i, div.denom5 {
                 color: ${colors.tagColor5} !important;
             }
-            i.denom6, span.denom6 i {
+            i.denom6, span.denom6 i, div.denom6 {
                 color: ${colors.tagColor6} !important;
             }
-            i.denom7, span.denom7 i {
+            i.denom7, span.denom7 i, div.denom7 {
                 color: ${colors.tagColor7} !important;
             }
-            i.denom8, span.denom8 i {
+            i.denom8, span.denom8 i, div.denom8 {
                 color: ${colors.tagColor8} !important;
             }
-            i.denom9, span.denom9 i {
+            i.denom9, span.denom9 i, div.denom9 {
                 color: ${colors.tagColor9} !important;
             }
         `, "final-whistle-custom-tag-colors");
