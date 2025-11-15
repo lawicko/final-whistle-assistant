4ea7f0df27ad91fa787dfa82e848ff7917575398
Jakub Lawicki <lawicko+github@gmail.com>
2025-11-15 20:46:09 +0100

    Added minimal filtering #49

--- src/analysis.js
+++ src/analysis.js
@@ -15,98 +15,171 @@ browser.runtime.onMessage.addListener((msg) => {
             return
         }
 
-        for (opportunity of analysisData) {
-            console.info("opportunity:", opportunity)
+        const formElement = document.createElement('form')
+        formElement.id = "filtersForm"
+        document.body.appendChild(formElement)
 
-            const minuteElement = document.createElement('span')
-            minuteElement.classList.add('engine-minute')
-            minuteElement.textContent = `Minute ${opportunity.minute}`
-            document.body.appendChild(minuteElement)
-            insertBR()
+        // Team selector
+        const teams = [...new Set(analysisData.map(e => e.team))];
+        const select = document.createElement('select');
+        select.id = "teamSelect";
+        select.name = "teamFilter"; 
+        const defaultOption = document.createElement('option');
+        defaultOption.value = "";
+        defaultOption.textContent = "Both teams";
+        select.appendChild(defaultOption);
+        teams.forEach(team => {
+            const option = document.createElement('option');
+            option.value = team;
+            option.textContent = team;
+            select.appendChild(option);
+        });
+        select.addEventListener("change", () => {
+            displayOpportunities(analysisData, getFilters())
+        });
+        formElement.appendChild(select);
 
-            const teamElement = document.createElement('span')
-            teamElement.classList.add('engine-team')
-            teamElement.textContent = opportunity.team
-            document.body.appendChild(teamElement)
-            insertBR()
+        // Creator checkbox
+        const label = document.createElement('label');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.name = 'showCreatedBy'
+        checkbox.checked = false;
 
+        label.appendChild(checkbox);
+        label.appendChild(document.createTextNode(" Show created by"));
+
+        formElement.appendChild(label);
+
+        checkbox.addEventListener("change", () => {
+            displayOpportunities(analysisData, getFilters())
+        });
+
+        insertBR(document.body)
+
+        displayOpportunities(analysisData, getFilters())
+    }
+});
+
+function getFilters() {
+    const form = document.getElementById("filtersForm");
+    console.info("form:",form)
+    const fd = new FormData(form);
+    console.info("new FormData(form):",fd)
+    console.info("will return filters:", Object.fromEntries(fd.entries()))
+    return Object.fromEntries(fd.entries());
+}
+
+function displayOpportunities(analysisData, config) {
+
+    const containerID = "resultContainer"
+    const existingContainer = document.body.querySelector(`#${containerID}`)
+    if (existingContainer) {
+        existingContainer.remove()
+    }
+
+    const container = document.createElement('div')
+    container.id = containerID
+    document.body.appendChild(container)
+
+    let opportunities = analysisData
+    if (config.teamFilter != "") {
+        opportunities = analysisData.filter(opportunity => opportunity.team === config.teamFilter)
+    }
+
+    for (const opportunity of opportunities) {
+        console.info("opportunity:", opportunity)
+
+        const minuteElement = document.createElement('span')
+        minuteElement.classList.add('engine-minute')
+        minuteElement.textContent = `Minute ${opportunity.minute}`
+        container.appendChild(minuteElement)
+        insertBR(container)
+
+        const teamElement = document.createElement('span')
+        teamElement.classList.add('engine-team')
+        teamElement.textContent = opportunity.team
+        container.appendChild(teamElement)
+        insertBR(container)
+
+        if (config.showCreatedBy) {
             const creatorElement = document.createElement('span')
             creatorElement.textContent = `Created by `
             insertPlayerLink(opportunity.creator, creatorElement)
             creatorElement.append(" [" + opportunity.creator.position + "]")
-            document.body.appendChild(creatorElement)
-            insertBR()
-
-            const context = opportunity.context
-            if (context && context != "block") {
-                const contextElement = document.createElement('span')
-                contextElement.classList.add("engine-corner")
-                contextElement.textContent = titleCase(context)
-                document.body.appendChild(contextElement)
-                insertBR()
-            }
-
-            const actionElement = document.createElement('span')
-            insertPlayerLink(opportunity.potentialAssistant, actionElement)
-            actionElement.append(" [" + opportunity.potentialAssistant.position + "]")
-            actionElement.append(` tried ${opportunity.passType} pass to `)
-            insertPlayerLink(opportunity.intendedReceiver, actionElement)
-            actionElement.append(" [" + opportunity.intendedReceiver.position + "]")
-
-            switch (opportunity.outcome) {
-                case "cleared":
-                    let potentialBlock = ""
-                    if (context && context === "block") {
-                        potentialBlock = "blocked and later "
-                    }
-                    actionElement.append(`, but the ball was ${potentialBlock}cleared by `)
-                    insertPlayerLink(opportunity.stopper, actionElement)
-                    actionElement.append(" [" + opportunity.stopper.position + "]")
-                    break;
-                case "caught":
-                    actionElement.append(`, but the ball was caught by `)
-                    insertPlayerLink(opportunity.stopper, actionElement)
-                    actionElement.append(" [" + opportunity.stopper.position + "]")
-                    break;
-                case "corner":
-                    actionElement.append(`, but the ball was sent to corner by `)
-                    insertPlayerLink(opportunity.stopper, actionElement)
-                    actionElement.append(" [" + opportunity.stopper.position + "]")
-                    break;
-                case "offensiveFoul":
-                    actionElement.append(`, but he committed an offensive foul in the process.`)
-                    break;
-                case "interception":
-                    actionElement.append(`, but `)
-                    insertPlayerLink(opportunity.stopper, actionElement)
-                    actionElement.append(" [" + opportunity.stopper.position + "] intercepted the ball.")
-                    break;
-                case "goal":
-                    actionElement.append(`, and he successfully scored a goal.`)
-                    break;
-                default:
-                    actionElement.append(`, UNKNOWN STOPPAGE ACTION `)
-                    break;
-            }
-            document.body.appendChild(actionElement)
-
-            insertBR()
-            insertBR()
+            container.appendChild(creatorElement)
+            insertBR(container)
         }
+
+        const context = opportunity.context
+        if (context && context != "block") {
+            const contextElement = document.createElement('span')
+            contextElement.classList.add("engine-corner")
+            contextElement.textContent = titleCase(context)
+            container.appendChild(contextElement)
+            insertBR(container)
+        }
+
+        const actionElement = document.createElement('span')
+        insertPlayerLink(opportunity.potentialAssistant, actionElement)
+        actionElement.append(" [" + opportunity.potentialAssistant.position + "]")
+        actionElement.append(` tried ${opportunity.passType} pass to `)
+        insertPlayerLink(opportunity.intendedReceiver, actionElement)
+        actionElement.append(" [" + opportunity.intendedReceiver.position + "]")
+
+        switch (opportunity.outcome) {
+            case "cleared":
+                let potentialBlock = ""
+                if (context && context === "block") {
+                    potentialBlock = "blocked and later "
+                }
+                actionElement.append(`, but the ball was ${potentialBlock}cleared by `)
+                insertPlayerLink(opportunity.stopper, actionElement)
+                actionElement.append(" [" + opportunity.stopper.position + "]")
+                break;
+            case "caught":
+                actionElement.append(`, but the ball was caught by `)
+                insertPlayerLink(opportunity.stopper, actionElement)
+                actionElement.append(" [" + opportunity.stopper.position + "]")
+                break;
+            case "corner":
+                actionElement.append(`, but the ball was sent to corner by `)
+                insertPlayerLink(opportunity.stopper, actionElement)
+                actionElement.append(" [" + opportunity.stopper.position + "]")
+                break;
+            case "offensiveFoul":
+                actionElement.append(`, but he committed an offensive foul in the process.`)
+                break;
+            case "interception":
+                actionElement.append(`, but `)
+                insertPlayerLink(opportunity.stopper, actionElement)
+                actionElement.append(" [" + opportunity.stopper.position + "] intercepted the ball.")
+                break;
+            case "goal":
+                actionElement.append(`, and he successfully scored a goal.`)
+                break;
+            default:
+                actionElement.append(`, UNKNOWN STOPPAGE ACTION `)
+                break;
+        }
+        container.appendChild(actionElement)
+
+        insertBR(container)
+        insertBR(container)
     }
-});
+}
 
 const capitalizeFirst = s => s ? s[0].toUpperCase() + s.slice(1) : "";
 
 function titleCase(str) {
-  return str
-    .split(" ")
-    .map(w => capitalizeFirst(w))
-    .join(" ");
+    return str
+        .split(" ")
+        .map(w => capitalizeFirst(w))
+        .join(" ");
 }
 
-function insertBR() {
-    document.body.appendChild(document.createElement('br'))
+function insertBR(targetElement) {
+    targetElement.appendChild(document.createElement('br'))
 }
 
 function insertPlayerLink(player, target) {
